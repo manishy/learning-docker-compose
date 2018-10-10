720c0bf586d57cd774141b6d0f27e4c568c93f3f
kaskichandrakant <chandk@thoughtworks.com>
2018-10-10 15:52:12 +0530

    Sharing Fun

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const network = require('network');
 const app=express();
 const PORT=8000;
 app.use(express.json());
@@ -11,4 +12,8 @@ app.get('/',(req,res)=>{
 app.get('/nextPage',(req,res)=>{
     res.send("<h1>HELLO WORLD IN DIFF PAGE<h1>");
 })
+app.get('/ip',(req,res)=>{
+    network.get_active_interface((err,obj)=>res.send(obj["ip_address"]))
+})
+
 console.log(`listening at Port${PORT}`);
